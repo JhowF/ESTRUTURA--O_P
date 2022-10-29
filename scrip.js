8586ed9099fbf34692d3073036814bf55a6c5faf
JhowF <jonathanferreirar@gmail.com>
2022-10-29 00:36:17 -0300

    mechendoNocheckBox

--- scrip.js
+++ scrip.js
@@ -1,7 +1,38 @@
-let nome = window.prompt(' Digite seu nome:')
 
-console.log(nome);
+function k (){
 
-if (nome == "jonathan"){
-    alert("Deu bom man")
-};
+let nome = window.prompt(' Digite seu nome:');
+
+let usuario = document.querySelector('#NomeUsuario');
+
+console.log(usuario);
+
+usuario.innerHTML = nome;}
+
+
+// let block = document.querySelector('.Checkbox');
+
+// for (var i = 0; i < block.length; i++) {
+// if (block[i].checked==true){
+
+// console.log(block[i].id)
+// }}
+
+let img = document.querySelector('#img');
+
+function obterMarcados() {
+    let img = document.querySelector('#img');
+    var listaMarcados = document.getElementsByTagName("input");
+    for (loop = 0; loop < listaMarcados.length; loop++) {
+       var item = listaMarcados[loop];
+       if (item.type == "checkbox" && item.checked) {
+         alert(img.value);
+       }
+    }
+  }
+
+  let bt = document.querySelector('#Button');
+
+  bt.addEventListener('click', obterMarcados)
+
+  
